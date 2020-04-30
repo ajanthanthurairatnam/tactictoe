ce99a0f047977efdf1c7cfd1f7ce01b36255e941
ajanthan <ajanthan.kt@gmail.com>
2020-04-30 16:59:51 +1000

    Build

---
+++ tactictoe/build/precache-manifest.0f60f27f0f62240b6cfc7e3952aac156.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "846f17e2eff5d2ae6e8aaaebef0cc476",
+    "url": "/index.html"
+  },
+  {
+    "revision": "052a2166c4ab392a5d4c",
+    "url": "/static/css/main.fe621161.chunk.css"
+  },
+  {
+    "revision": "dae070553879c6d0b182",
+    "url": "/static/js/2.257a172b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.257a172b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "052a2166c4ab392a5d4c",
+    "url": "/static/js/main.ec35993b.chunk.js"
+  },
+  {
+    "revision": "6d17acc163fa05097929",
+    "url": "/static/js/runtime-main.2ff11c38.js"
+  }
+]);
